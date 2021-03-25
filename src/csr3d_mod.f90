bc1cf7b2bdffddcc613c1dc3ac213fde92d03a0b
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-03-24 21:58:11 -0700

    Test program, density derivative. Note: Fails to link FFTW

--- src/csr3d_mod.f90
+++ src/csr3d_mod.f90
@@ -3,7 +3,8 @@ module csr3d_mod
 
 use, intrinsic :: iso_fortran_env
 use elliptic_integral_mod, only : ellipinc 
-!use fft_mod 
+use fft_mod, only : ccfft3d
+
 
 implicit none
 
@@ -13,9 +14,7 @@ integer, parameter, private :: dp = REAL64
 !integer, parameter, private :: qp = REAL128
 
 type mesh3d_struct
-  integer :: nlo(3) = [ 1,  1,  1]       ! Lowest  grid index in x, y, z (m) of rho and the quantity being computed (phi or E)
-  integer :: nhi(3) = [64, 64, 64]       ! Highest grid index in x, y, z (m) of rho and the quantity being computed (phi or E)
-  integer :: npad(3) = [ 1,  1,  1]      ! Array padding for cyclic convolution
+  integer :: size(3) = [64, 64, 64]     ! Grid shape
   real(dp) :: min(3)                    ! Minimim in each dimension
   real(dp) :: max(3)                    ! Maximum in each dimension
   real(dp) :: delta(3)                  ! Grid spacing
@@ -23,9 +22,9 @@ type mesh3d_struct
   real(dp) :: rho                       ! bending radius (positive)
   real(dp) :: charge                    ! Total charge on mesh
   real(dp), allocatable, dimension(:,:,:) :: density        ! Charge density grid
-  real(dp), allocatable, dimension(:,:,:) :: phi        ! electric potential grid
-  real(dp), allocatable, dimension(:,:,:,:) :: efield   ! electric field grid
-  real(dp), allocatable, dimension(:,:,:,:) :: bfield   ! magnetic field grid
+  real(dp), allocatable, dimension(:,:,:) :: density_prime        ! d/dz density
+  real(dp), allocatable, dimension(:,:,:,:) :: wake        ! wake grid
+
 end type
 
 
@@ -45,17 +44,16 @@ subroutine print_mesh3d(mesh3d)
 type(mesh3d_struct) :: mesh3d
 print *, '------------------------'
 print *, 'Mesh: '
-print *, 'nlo: ', mesh3d%nlo
-print *, 'nhi: ', mesh3d%nhi
+print *, 'size: ', mesh3d%size
 print *, 'min: ', mesh3d%min
 print *, 'max: ', mesh3d%max
 print *, 'delta: ', mesh3d%delta
 print *, 'gamma: ', mesh3d%gamma
+print *, 'rho: ', mesh3d%rho
 print *, 'charge: ', mesh3d%charge
 if (allocated(mesh3d%density)) print *, 'density allocated'
-if (allocated(mesh3d%phi)) print *, 'phi allocated'
-if (allocated(mesh3d%efield)) print *, 'efield allocated'
-if (allocated(mesh3d%bfield)) print *, 'bfield allocated'
+if (allocated(mesh3d%density_prime)) print *, 'density_prime allocated'
+if (allocated(mesh3d%wake)) print *, 'wake allocated'
 print *, '------------------------'
 end subroutine
 
@@ -144,20 +142,12 @@ end subroutine
 !     
 !
 !-
-subroutine space_charge_3d(mesh3d, offset, at_cathode, calc_bfield)
+subroutine csr3d_steady_state(mesh3d, offset)
 type(mesh3d_struct) :: mesh3d
 real(dp), allocatable, dimension(:,:,:,:) :: image_efield   ! electric field grid
 real(dp), optional :: offset(3)
 real(dp) :: offset0(3), beta
 real(dp), parameter :: c_light = 299792458.0
-logical, optional :: at_cathode, calc_bfield
-logical :: bcalc
-
-if (present(calc_bfield)) then
-  bcalc = calc_bfield
-else
-  bcalc = .false.
-endif
 
 if (.not. present(offset)) then
   offset0 = 0
@@ -166,11 +156,45 @@ else
 endif
 
 ! Free space field
-call csr3d_steady_state_solver(mesh3d%density, mesh3d%gamma, mesh3d%rho, mesh3d%delta, efield=mesh3d%efield, offset=offset0)
+call csr3d_steady_state_solver(mesh3d%density, mesh3d%gamma, mesh3d%rho, mesh3d%delta, mesh3d%wake, offset=offset0)
 
 
+end subroutine csr3d_steady_state
+
+
+
+
+!------------------------------------------------------------------------
+!+
+! Subroutine calc_density_derivative(density, density_prime, dz)
+!
+! Input:
+!
+!
+! Output:
+!
+!-
+subroutine calc_density_derivative(density, density_prime, dz)
+real(dp), intent(in), dimension(:,:,:) :: density
+real(dp), intent(in) :: dz
+complex(dp), allocatable, dimension(:,:,:), intent(out) :: density_prime
+integer :: nz
+
+! Sizes
+nz = size(density, 3)
+
+! Allocate double-sized arrays. This array will be placed in the first octant
+allocate( density_prime( 2*size(density, 1), 2*size(density, 2), 2*size(density, 3) ))
+density_prime = 0 ! Zero out everything
+
+! Endpoints: Simple backward and forward differences
+density_prime(:,:,1)  = (density(:,:,2)  - density(:,:,1))/dz
+density_prime(:,:,nz) = (density(:,:,nz) - density(:,:,nz-1))/dz
+
+! Central differences
+density_prime(:,:,2:nz-1) =  (density(:,:,3:nz) - density(:,:,1:nz-2))/(2*dz)
+!
 end subroutine
-
 
 
 !------------------------------------------------------------------------
@@ -209,21 +233,17 @@ end subroutine
 !
 !
 ! Notes: 
-!   The magnetic field components can be calculated by:
-!     Bx = -(beta/c) * Ey
-!     By =  (beta/c) * Ex
-!     Bz = 0
 !
 !-
-subroutine csr3d_steady_state_solver(density, gamma, rho, delta, efield, offset)
+subroutine csr3d_steady_state_solver(density, gamma, rho, delta, wake, offset)
 
 
 real(dp), intent(in), dimension(:,:,:) :: density
 real(dp), intent(in) :: gamma, rho, delta(3)
-real(dp), optional, intent(out), dimension(:,:,:,:) :: efield
+real(dp), optional, intent(out), dimension(:,:,:,:) :: wake
 real(dp), intent(in), optional :: offset(3)
 ! internal arrays
-complex(dp), allocatable, dimension(:,:,:) :: cdensity, cgrn
+complex(dp), allocatable, dimension(:,:,:) :: complex_density_prime, cgrn
 real(dp) :: factr, offset0=0
 real(dp), parameter :: clight=299792458.0
 real(dp), parameter :: fpei=299792458.0**2*1.00000000055d-7  ! this is 1/(4 pi eps0) after the 2019 SI changes
@@ -236,25 +256,23 @@ nx = size(density, 1); ny = size(density, 2); nz = size(density, 3)
 nx2 = 2*nx; ny2 = 2*ny; nz2 = 2*nz; 
 
 ! Allocate complex scratch arrays
-allocate(cdensity(nx2, ny2, nz2))
 allocate(cgrn(nx2, ny2, nz2))
 
-! density -> cdensity -> FFT(cdensity)
-cdensity = 0
-cdensity(1:nx, 1:ny, 1:nz) = rho ! Place in one octant
-!call ccfft3d(cdensity, cdensity, [1,1,1], nx2, ny2, nz2, 0) 
+! density -> d/dz density -> FFT 
+! This will calculate d/dz density and place in the complex array
+call calc_density_derivative(density, complex_density_prime, delta(3))
+call ccfft3d(complex_density_prime, complex_density_prime, [1,1,1], nx2, ny2, nz2, 0) 
 
-! Loop over phi, Ex, Ey, Ez
-do icomp=0, 3
-  if ((icomp == 1) .and. (.not. present(efield))) exit
+! Loop over Gx, Gy, Gs
+do icomp=1, 3
 
   call get_cgrn_csr3d(cgrn, delta, gamma, rho, icomp, offset=offset)
   
   !  cgrn -> FFT(cgrn)
- ! call ccfft3d(cgrn, cgrn, [1,1,1], nx2, ny2, nz2, 0)  
+  call ccfft3d(cgrn, cgrn, [1,1,1], nx2, ny2, nz2, 0)  
   
   ! Multiply FFT'd arrays, re-use cgrn
-  cgrn=cdensity*cgrn
+  cgrn=complex_density_prime*cgrn
 
   ! Inverse FFT
   !call ccfft3d(cgrn, cgrn, [-1,-1,-1], nx2, ny2, nz2, 0)  
@@ -268,7 +286,7 @@ do icomp=0, 3
   ! ???? Factor
   factr = fpei/(nx2*ny2*nz2)
   
-  efield(:,:,:,icomp) = factr * real(cgrn(1+ishift:nx+ishift, 1+jshift:ny+jshift, 1+kshift:nz+kshift), dp)
+  wake(:,:,:,icomp) = factr * real(cgrn(1+ishift:nx+ishift, 1+jshift:ny+jshift, 1+kshift:nz+kshift), dp)
 
     
 enddo
@@ -278,7 +296,7 @@ end subroutine csr3d_steady_state_solver
 
 !------------------------------------------------------------------------
 !+
-! Subroutine osc_get_cgrn_freespace(cgrn, delta, gamma, icomp, offset)
+! Subroutine get_cgrn_csr3d(cgrn, delta, gamma, rho, icomp, offset)
 !
 ! Computes the free space Green function on a mesh with given spacings in the lab frame.
 ! The computation is performed in the rest fram by boosting the coordinates by gamma.
@@ -288,11 +306,12 @@ end subroutine csr3d_steady_state_solver
 !   cgrn         -- COMPLEX128(:,:,:): pre-allocated array 
 !   delta        -- REAL64(3): vector of grid spacings dx, dy, dz
 !   gamma        -- REAL64: relativistic gamma
-!   icomp        -- integer: Field component requested:
-!                        0: phi (scalar potential)
-!                        1: Ex
-!                        2: Ey
-!                        3: Ez
+!   rho          -- REAL64: bending radius (must be positive)
+!   icomp        -- integer: Wake component requested:
+!
+!                        1: Wx
+!                        2: Wy
+!                        3: Ws
 !   offset        -- real(3), optional: Offset coordinates for the center of the grid in [m]. 
 !                    Default: (0,0,0)
 !                        For example, an offset of (0,0,10) can be used to compute 
@@ -303,10 +322,11 @@ end subroutine csr3d_steady_state_solver
 !   
 !                
 ! Notes:
-!   Internally, dz -> dz*gamma.             
-!   For efficients, the indefinite functions lafun2, xlafun2 are actually evaluated 
-!   on a grid slightly offset by -dx/2, -dy/2, -dz/2,
-!   and these points are used to evaluate the integral with array addition and subtraction. 
+!   Internally, mesh spacings in scaled units
+!   x - > x/rho
+!   y ->  y/rho
+!   z ->  z/(2*rho)   
+!
 !
 !-
 subroutine get_cgrn_csr3d(cgrn, delta, gamma, rho, icomp, offset)
@@ -329,7 +349,6 @@ integer :: i,j,k, isize, jsize, ksize
 ! z ->  z/(2*rho)
 
 dx=delta(1)/rho; dy=delta(2)/rho; dz=delta(3)/(2*rho)
-
 
 ! ????
 factor = 1.0 /(dx*dy*dz)
@@ -360,8 +379,10 @@ do k = 0, ksize-1
    do i=0, isize-1
      u = i*dx + umin
      
-     if(icomp == 0) gval=psi_s(u, w, v, gamma)*factor
-     cgrn(i,j,k)= cmplx(gval, 0, dp)
+     !gval=psi(u, w, v, gamma, icomp)*factor
+     !cgrn(i,j,k)= cmplx(gval, 0, dp)
+     
+     cgrn(i,j,k)=  psi(u, w, v, gamma, icomp)*factor
      
     enddo
   enddo
@@ -412,13 +433,19 @@ end function psi_s
 !
 ! Eq. 24 from Ref[X] without the prefactor e beta^2 / (2 rho^2)
 ! This is actually psi_x_hat
+!
+!
+!
+! icomp : integer
+!          1: x
+!          2: y
+!          3: s
 !-
-elemental real(dp) function psi_x(x, y, z, gamma)
+elemental real(dp) function psi(x, y, z, gamma, icomp)
 real(dp), intent(in) :: x, y, z, gamma
+integer, intent(in) :: icomp
 real(dp) :: beta, beta2, kap, alp
 real(dp) :: sin2a, cos2a, kap2, sin2a2, x2, y2, y4, xp, xp2, xy, xy2, f1, f2, arg2, f, e
-
-
 
 beta2 = 1-1/gamma*2
 beta = sqrt(beta2)
@@ -426,6 +453,11 @@ beta = sqrt(beta2)
 alp = alpha(x, y, z, gamma)
 kap = 2*(alp - z)/beta ! Simpler form of kappa
 !kap = sqrt(x**2 + y**2 + 4*(1+x) * sin(alp)**2) 
+
+if (icomp == 3) then
+    psi = (cos(2*alp) - 1/(1+x)) / (kap - beta*(1+x)*sin(2*alp) )
+    return
+endif
 
 ! Common patterns
 sin2a = sin(2*alp)
@@ -447,28 +479,32 @@ f1 = 2 + 2*x +x2
 f2 = (2+x)**2
 arg2 = -4 * xp / xy2 
 
-
 ! Elliptic integrals of the first and second kind F(phi|m), E(phi|m)
 call ellipinc(alp, arg2, F, E)
-    
-! psi_x (actually psi_x_hat that includes the psi_phi term)
+
+if (icomp == 1) then
+! psi_x    
+! psi_x is actually psi_x_hat that includes the psi_phi term
 ! There is an extra ] in the numerator of the second term. All terms should multiply E. 
 
-psi_x = f1*F / (xp*xy) - (x2*f2 + y2*f1)*E / (xp*(y2+f2)*xy)  &
+    psi = f1*F / (xp*xy) - (x2*f2 + y2*f1)*E / (xp*(y2+f2)*xy)  &
         + ( kap2 - 2*beta2*xp2 + beta2*xp*f1*cos2a  ) / (beta *xp*(kap2 - beta2*xp2*sin2a2)) &
         + kap*( y4 - x2*f2 - 2*beta2*y2*xp2 )*sin2a / ( xy2*(y2 + f2)*(kap2-beta2*xp2*sin2a2)  ) &
         + kap*beta2*xp*( x2*f2 + y2*f1 )*sin2a*cos2a / ( xy2*(y2+f2)*(kap2-beta2*xp2*sin2a2)  ) &
         - (2/beta2)* F/xy ! Include the phi term      
-        
-! TODO: function psi_y
-!psi_y = y * ( &
-!        F/xy - (x*(2+x)+y2)*E / ((y2+f2)*xy) &
-!        - beta*(1-xp*cos2a) / (kap2-beta2*xp2*sin2a2) &
-!        + kap*xp*( -(2+beta2)*y2 + (-2+beta2)*x*(2+x) ) * sin2a / ( (y4 + x2*f2 + 2*y2*f1)*( kap2-beta2*xp2*sin2a2 ) ) &
-!        + kap*beta2*xp2*(y2 + x*(2+x))*sin2a*cos2a / ( ( y4 + x2*f2 + 2*y2*f1)*(kap2 -beta2*xp2*sin2a2)  ) &
-!        )        
 
-end function psi_x
+elseif (icomp == 2) then
+! psi_y
+    psi = y * ( &
+        F/xy - (x*(2+x)+y2)*E / ((y2+f2)*xy) &
+        - beta*(1-xp*cos2a) / (kap2-beta2*xp2*sin2a2) &
+        + kap*xp*( -(2+beta2)*y2 + (-2+beta2)*x*(2+x) ) * sin2a / ( (y4 + x2*f2 + 2*y2*f1)*( kap2-beta2*xp2*sin2a2 ) ) &
+        + kap*beta2*xp2*(y2 + x*(2+x))*sin2a*cos2a / ( ( y4 + x2*f2 + 2*y2*f1)*(kap2 -beta2*xp2*sin2a2)  ) &
+        )        
+endif
+
+
+end function psi
 
 
 
--- tests/test_csr3d.f90
+++ tests/test_csr3d.f90
@@ -1,9 +1,142 @@
 program test_csr3d
 
+use, intrinsic :: iso_fortran_env
 use csr3d_mod
 
 implicit none
 
+type(mesh3d_struct) :: mesh3d
+integer, parameter :: dp = REAL64
+
+
+
+real(dp) :: gamma, rho, sigmas(3), gauss_cutoff
+integer ::  sizes(3)
+
+
+! File handling
+character(200) :: in_file
+integer :: open_status, namelist_file
+namelist / csr3d_test_params / &
+  sizes, gamma, rho, &
+  sigmas, gauss_cutoff
+
+!Namelist defaults
+gamma = 500
+rho = 1
+sizes = [64, 32, 128]
+sigmas = [10.0e-3_dp, 10.0e-3_dp, 10.0e-3_dp]
+gauss_cutoff = 5.0
+
+!Read namelist
+in_file = 'test_opensc.in'
+if (command_argument_count() > 0) call get_command_argument(1, in_file)
+open(newunit=namelist_file, file = in_file, status = 'old', iostat=open_status)
+if (open_status /= 0) then
+  print *, 'Input file missing: ', in_file
+  print *, 'Using defaults'
+else 
+  read (namelist_file, nml = csr3d_test_params)
+  close (namelist_file)
+endif
+
+print *, '------------------------'
+write(*, csr3d_test_params)
+print *, '------------------------'
+
+
+
+mesh3d%size = sizes
+call gaussian_mesh(mesh3d, sigmas, gauss_cutoff)
+
+
 call csr3d_calc()
+call ellipinc_test()
+
+
+
+
+contains
+
+
+
+
+
+
+! Fills mesh with Gaussian data
+subroutine gaussian_mesh(mesh3d, sigmas, cutoff)
+type(mesh3d_struct) :: mesh3d
+real(dp) :: x, y, z, sigmas(3), cutoff
+real(dp) :: dx, dy, dz, xmin, ymin, zmin, norm
+integer :: i, j, k, isize, jsize, ksize
+
+if (.not. allocated(mesh3d%density)) then
+  allocate(mesh3d%density(mesh3d%size(1), mesh3d%size(2), mesh3d%size(3)))
+  allocate(mesh3d%density_prime(mesh3d%size(1), mesh3d%size(2), mesh3d%size(3)))
+  allocate(mesh3d%wake(mesh3d%size(1), mesh3d%size(2), mesh3d%size(3), 3))   
+endif
+
+
+! Fill mesh
+mesh3d%min   = -cutoff*sigmas
+mesh3d%max   =  cutoff*sigmas
+mesh3d%delta = (mesh3d%max - mesh3d%min)/(mesh3d%size-1)
+
+! Convenient local variables. 
+isize = mesh3d%size(1)
+jsize = mesh3d%size(2)
+ksize = mesh3d%size(3)
+
+dx = mesh3d%delta(1)
+dy = mesh3d%delta(2)
+dz = mesh3d%delta(3)
+xmin = mesh3d%min(1)
+ymin = mesh3d%min(2)
+zmin = mesh3d%min(3)
+
+do k = 0, ksize-1
+  z = k*dz + zmin
+
+  do j=0, jsize-1
+    y=j*dy + ymin
+
+   do i=0, isize-1
+     x = i*dx + xmin
+     
+     mesh3d%density(i,j,k)= gauss3_prime(x, y, z, sigmas(1), sigmas(2), sigmas(3))
+     
+    enddo
+  enddo
+enddo
+
+! Normalize
+norm = sum(mesh3d%density) * dx * dy * dz
+mesh3d%density  = mesh3d%density / norm
+
+
+end subroutine 
+
+! Gaussian
+elemental real(dp) function gauss(x, sigma)
+real(dp), intent(in):: x, sigma
+real(dp), parameter :: pi = 4.0_dp*atan2(1.0_dp,1.0_dp)
+gauss =  1/(sigma * sqrt(2 * pi)) * exp( - (x)**2 / (2 * sigma**2) )
+end function
+
+! 3D Gaussian
+elemental real(dp) function gauss3(x, y, z, sigma_x, sigma_y, sigma_z)
+real(dp), intent(in):: x, y, z, sigma_x, sigma_y, sigma_z
+real(dp), parameter :: pi = 4.0_dp*atan2(1.0_dp,1.0_dp)
+gauss3 =  gauss(x, sigma_x)*gauss(y, sigma_y)*gauss(z, sigma_z)
+end function
+
+! 3D d/dz Gaussian
+elemental real(dp) function gauss3_prime(x, y, z, sigma_x, sigma_y, sigma_z)
+real(dp), intent(in):: x, y, z, sigma_x, sigma_y, sigma_z
+real(dp), parameter :: pi = 4.0_dp*atan2(1.0_dp,1.0_dp)
+gauss3_prime =  gauss(x, sigma_x)*gauss(y, sigma_y)*gauss(z, sigma_z)*(-z/sigma_z**2)
+end function
+
+
 
 end program
